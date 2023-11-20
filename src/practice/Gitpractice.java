2ed966c89a97a4fb3d4d9f632caf12be7cdfd833
vishnuganesh1992 <vishnu.dina@gmail.com>
2023-11-20 07:20:47 +0530

    keerthu completed task

--- src/practice/Gitpractice.java
+++ src/practice/Gitpractice.java
@@ -4,6 +4,8 @@ public class Gitpractice {
 	public static void main(String[] args) {
 		for (int i = 0; i < 10; i++) {
 			System.out.println(i);
+			System.out.println("Keerthu");
+			
 			
 		}
 	}
